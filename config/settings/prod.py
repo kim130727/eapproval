6d60159e55004eae1ad18c1a03cf26dc669d3e28
kim130727 <kim130727@gmail.com>
2026-02-28 12:57:50 +0900

    로깅 및 도메인추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,10 @@
 from .base import *
 
-ALLOWED_HOSTS = ['54.116.98.10']
+ALLOWED_HOSTS = [
+    "saenuree3.duckdns.org",
+    "54.116.98.10",
+    "localhost",
+    "ec2-54-116-98-10.ap-northeast-2.compute.amazonaws.com",
+]
+
+DEBUG = False
