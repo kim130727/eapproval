84faeb5d24c74ff5ab1759308b6d1954750e74f1
kim130727 <kim130727@gmail.com>
2026-03-30 12:46:25 +0900

    db host local postgre와 겹치지 않도록

--- config/settings/base.py
+++ config/settings/base.py
@@ -65,7 +65,7 @@ if db_engine == "postgresql":
             "NAME": os.getenv("DB_NAME"),
             "USER": os.getenv("DB_USER"),
             "PASSWORD": os.getenv("DB_PASSWORD"),
-            "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+            "HOST": os.getenv("DB_HOST", ""),
             "PORT": os.getenv("DB_PORT", "5432"),
         }
     }
